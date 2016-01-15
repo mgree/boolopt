53d60ed3050f1cb3526aa33ff0cc17b4af94aacd
Michael Greenberg <michael.m.greenberg@gmail.com>
2016-01-15 15:31:07 -0800

    boolopt 1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='boolopt',
+      version='1.0',
+      description='Boolean expression minimizer',
+      author='Michael Greenberg',
+      author_email='mike@weaselhat.com',
+      url='http://www.weaselhat.com/boolopt/',
+      py_modules=['boolopt'])
+
